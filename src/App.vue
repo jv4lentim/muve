b7c341c9be83e7539ede96891f0a49b4dfb91521
Valentim <joaovictorvalentim@gmail.com>
2020-01-26 19:32:43 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
+body {
+  background-color: #0F1014;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 900px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header-container">
+    <h2 class="logo">Muve</h2>
+    <div class="search-bar">
+      <input type="text" placeholder="Pesquise por um filme" >
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+  .header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+
+    .logo {
+      color: #ffffff;
+      font-size: 31px;
+      font-weight: bold;
+    }
+
+    .search-bar {
+      input {
+        padding: 10px;
+        border-radius: 15px;
+        border: none;
+      }
+    }
+  }
+</style>
